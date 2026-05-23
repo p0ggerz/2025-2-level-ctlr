c8539e954ecab8684d23a09cd0e5e6fb74de7481
Ksenia <ilovehotpupik@internet.ru>
2026-05-23 18:05:28 +0300

    F

--- lab_6_pipeline/pipeline.py
+++ lab_6_pipeline/pipeline.py
@@ -293,7 +293,6 @@ def main() -> None:
     Entrypoint for pipeline module.
     """
     corpus_manager = CorpusManager(path_to_raw_txt_data=ASSETS_PATH)
-    udpipe_analyzer = UDPipeAnalyzer()
     pipeline = TextProcessingPipeline(corpus_manager)
     pipeline.run()
 
